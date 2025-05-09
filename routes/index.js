f764745bef4d99b43cd8e4b937c830111edcd4d6
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-05-09 13:38:01 -0300

    chore: altera estrutura de pastas

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('Rota principal funcionando!');
 });
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,40 +1,19 @@
-require('dotenv').config();
 const express = require('express');
-const app = express();
 const db = require('./config/db');
-const path = require('path');
+require('dotenv').config();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
 
-db.connect()
-  .then(() => {
-    console.log('Conectado ao banco de dados PostgreSQL');
+app.get('/', async (req, res) => {
+  try {
+    const result = await db.query('SELECT NOW()');
+    res.send(`Hora atual no banco: ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send('Erro ao conectar com o banco.');
+  }
+});
 
-    app.use(express.json());
-
-    const userRoutes = require('./routes/userRoutes');
-    app.use('/users', userRoutes);
-
-    const frontendRoutes = require('./routes/frontRoutes');
-    app.use('/', frontendRoutes);
-
-    // Middleware para lidar com erros de rota não encontrada
-    app.use((req, res, next) => {
-      res.status(404).send('Página não encontrada');
-    });
-
-    // Middleware para lidar com erros internos do servidor
-    app.use((err, req, res, next) => {
-      console.error(err.stack);
-      res.status(500).send('Erro no servidor');
-    });
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
