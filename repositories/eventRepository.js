7811fdfd49ca305d64b707417afbeeabb316ee32
Stefanne Victória Soares <stefanne.soares@sou.inteli.edu.br>
2025-05-24 15:30:58 -0300

    fix: refatora o codigo

--- repositories/eventRepository.js
+++ repositories/eventRepository.js
@@ -9,15 +9,15 @@ class EventRepository {
 
     async findById(id) {
         const result = await db.query('SELECT nome, descricao, data, local, duracao_horas, entidade_id FROM evento WHERE id = $1', [id]);
-        return result.rows.length ? new Event(result.rows[0]) : null;
+        return result.rows.length ? new eventModel(result.rows[0]) : null;
     }
 
     async create(event) {
         try {
             const result = await db.query(
-            `INSERT INTO evento (nome, descricao, data, local, duracao_horas, entidade_id)
-            VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-            [event.nome, event.descricao, event.data, event.local, event.duracao_horas, event.entidade_id]
+                `INSERT INTO evento (nome, descricao, data, local, duracao_horas, entidade_id)
+                VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+                [event.nome, event.descricao, event.data, event.local, event.duracao_horas, event.entidade_id]
             );
             return new eventModel(result.rows[0]);
         } catch (error) {
@@ -27,17 +27,17 @@ class EventRepository {
     }
 
     async update(id, event) {
-    const result = await db.query(
-        `UPDATE evento
-        SET nome = $1, descricao = $2, data = $3, local = $4, duracao_horas = $5, entidade_id = $6
-        WHERE id = $7 RETURNING *`,
-        [event.nome, event.descricao, event.data, event.local, event.duracao_horas, event.entidade_id, id]
-    );
-    return result.rows.length ? new eventModel(result.rows[0]) : null;
+        const result = await db.query(
+            `UPDATE evento
+            SET nome = $1, descricao = $2, data = $3, local = $4, duracao_horas = $5, entidade_id = $6
+            WHERE id = $7 RETURNING *`,
+            [event.nome, event.descricao, event.data, event.local, event.duracao_horas, event.entidade_id, id]
+        );
+        return result.rows.length ? new eventModel(result.rows[0]) : null;
     }
 
     async delete(id) {
-    await db.query('DELETE FROM evento WHERE id = $1', [id]);
+        await db.query('DELETE FROM evento WHERE id = $1', [id]);
     }
 
 }
